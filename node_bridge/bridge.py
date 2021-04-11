9a371b0a2cf9bca8d740796e90996669b8bb29c6
Link Wu <1774241372@qq.com>
2021-04-11 23:31:13 +0800

    update dependency

--- node_bridge/bridge.py
+++ node_bridge/bridge.py
@@ -2,25 +2,31 @@
 
 import serial
 from serial.tools import list_ports
-import node_bridge.helpers
+import helpers
 from queue import Queue
 import time
 
+
 class NodeBridge():
-    
-    def __init__(self, type='serial', port=0):
-        self.type = type
-        if type=='serial':
+
+    def __init__(self, port_type='serial', port=0):
+        self.port_type = port_type
+        if port_type == 'serial':
             # Set default port
             port_list = list(list_ports.comports())
+            print('Available ports: ', [x.device for x in port_list])
             self.default_port = ''
-            if len(port_list) > 0:
-                self.default_port = port_list[port].device
+            if type(port) == str:
+                self.default_port = port
+            else:
+                if len(port_list) > 0:
+                    self.default_port = port_list[port].device
+        print('Communication port: ', self.default_port)
 
     def send(self, packet, port=None):
         if not port:
             port = self.default_port
-        with serial.Serial(port, 115200, timeout = 0.1) as ser:
+        with serial.Serial(port, 115200, timeout=0.1) as ser:
             ser.write(packet)
             return True
 
@@ -34,51 +40,49 @@ class NodeBridge():
                 while on_receive(ser.read(1)):
                     pass
                 while not send_queue.empty():
-                    a=send_queue.get()
+                    a = send_queue.get()
                     ser.write(a)
 
+
 if __name__ == '__main__':
-        import protocol
-        bridge = NodeBridge('serial', port=1)
-        protocol_data = protocol.NodeBridgeProtocol()
+    import protocol
+    bridge = NodeBridge('serial', port='COM9')
+    protocol_data = protocol.NodeBridgeProtocol()
 
-        # send
-        # while True:
-        #     for i in range(10):
-        #         test_data = protocol.create_protocol_data('autoaimData')
-        #         test_data['fire'] = True
-        #         test_data['pitch_angle_diff'] = 1.11*i
-        #         test_data['yaw_angle_diff'] = 2.22
-        #         test_packet = protocol.pack('autoaimData', test_data, seq=i)
-        #         print(test_packet)
-        #         bridge.send(test_packet)
-        #         time.sleep(0.1)
+    # send
+    while True:
+        for i in range(10):
+            test_data = protocol.create_protocol_data('chassisData')
+            test_data['vx'] = 1
+            test_packet = protocol.pack('chassisData', test_data, seq=i)
+            print(test_packet)
+            bridge.send(test_packet)
+            time.sleep(0.01)
 
-        # receive
-        # def on_receive_test(new_byte):
-        #     result = protocol_data.update(new_byte, verbose=True)
-        #     if 'result' in result:
-        #         return False
-        #     return True
-        # while True:
-        #     bridge.open(on_receive_test)
+    # receive
+    # def on_receive_test(new_byte):
+    #     result = protocol_data.update(new_byte, verbose=True)
+    #     if 'result' in result:
+    #         return False
+    #     return True
+    # while True:
+    #     bridge.open(on_receive_test)
 
-        # send while receive
-        i=0
-        queue_test = Queue()
-        def on_receive_test(new_byte):
-            global i,queue_test
-            # add heartbeat to queue
-            if i%5==0:
-                test_data = protocol.create_protocol_data('heartbeat')
-                test_packet = protocol.pack('heartbeat', test_data, seq=i)
-                queue_test.put(test_packet)
-            i = (i+1)%256
-            # receive protocol
-            result = protocol_data.update(new_byte, verbose=True)
-            if 'result' in result:
-                return False
-            return True
-        bridge.open(on_receive_test, send_queue=queue_test)
+    # send while receive
+    # i = 0
+    # queue_test = Queue()
 
-
+    # def on_receive_test(new_byte):
+    #     global i, queue_test
+    #     # add heartbeat to queue
+    #     if i % 5 == 0:
+    #         test_data = protocol.create_protocol_data('heartbeat')
+    #         test_packet = protocol.pack('heartbeat', test_data, seq=i)
+    #         queue_test.put(test_packet)
+    #     i = (i+1) % 256
+    #     # receive protocol
+    #     result = protocol_data.update(new_byte, verbose=True)
+    #     if 'result' in result:
+    #         return False
+    #     return True
+    # bridge.open(on_receive_test, send_queue=queue_test)
--- node_bridge/protocol.py
+++ node_bridge/protocol.py
@@ -3,7 +3,7 @@ import yaml
 import bitstring
 import math
 import re
-from node_bridge import helpers
+import helpers
 import os
 
 # load protocol info
@@ -11,18 +11,21 @@ with open(os.path.split(os.path.realpath(__file__))[0]+'/../dist/protocol.yaml',
     file_data = file.read()
     protocol_info_table = yaml.safe_load(file_data)
 
+
 def packet_to_dict(name, packet):
     data = {}
     format_string = ''
     for item in protocol_info_table[name]['struct']:
         new_data = []
-        for i in range(item.get('arraysize',1)):
+        for i in range(item.get('arraysize', 1)):
             if 'struct' in item:
                 size = protocol_info_table[item['struct']]['size']
                 new_data += [packet_to_dict(item['struct'], packet)]
             else:
-                size = math.ceil(int(re.match('.*:(?P<bitsize>\d*)', item['format']).group('bitsize'))/8)
-                new_data += bitstring.BitArray(bytes=packet).unpack(item['format'])
+                size = math.ceil(
+                    int(re.match('.*:(?P<bitsize>\d*)', item['format']).group('bitsize'))/8)
+                new_data += bitstring.BitArray(
+                    bytes=packet).unpack(item['format'])
             packet = packet[size:]
         if 'arraysize' in item:
             data[item['key']] = new_data
@@ -30,8 +33,10 @@ def packet_to_dict(name, packet):
             data[item['key']] = new_data[0]
     return data
 
+
 def create_protocol_data(name):
     return packet_to_dict(name, bytes([0]*protocol_info_table[name]['size']))
+
 
 def dict_to_packet(name, data):
     def generate_format_sequence(name, data):
@@ -44,7 +49,8 @@ def dict_to_packet(name, data):
                 data_list = [data[item['key']]]
             for new_data in data_list:
                 if 'struct' in item:
-                    item['format'], new_data = generate_format_sequence(item['struct'], new_data)
+                    item['format'], new_data = generate_format_sequence(
+                        item['struct'], new_data)
                     data_items += new_data
                 else:
                     data_items += [new_data]
@@ -52,6 +58,7 @@ def dict_to_packet(name, data):
         return (",".join(formats), data_items)
     format_string, data_items = generate_format_sequence(name, data)
     return bitstring.pack(format_string, *data_items)
+
 
 def unpack(packet, skip_crc8=False):
     # find SOF
@@ -64,24 +71,28 @@ def unpack(packet, skip_crc8=False):
     # check crc8
     if len(packet) < 5-skip_crc8:
         return {'unused_packet': unused_packet}
-    if not skip_crc8 and helpers.get_crc8_checksum(packet[0:4])!=packet[4]:
+    if not skip_crc8 and helpers.get_crc8_checksum(packet[0:4]) != packet[4]:
         return {'unused_packet': packet[5:], 'error': 'CRC8 Not Correct'}
     # check crc16
     protocol_data = {}
-    protocol_data['data_length'] = packet[1]+(packet[2]<<8)
+    protocol_data['data_length'] = packet[1]+(packet[2] << 8)
     packet_correct_size = 9-skip_crc8+protocol_data['data_length']
     if len(packet) < packet_correct_size:
         return {'unused_packet': unused_packet}
-    packet, unused_packet = (packet[0:packet_correct_size], packet[packet_correct_size:])
-    if not helpers.get_crc16_checksum(packet[0:-2])==packet[-2]+(packet[-1]<<8):
+    packet, unused_packet = (
+        packet[0:packet_correct_size], packet[packet_correct_size:])
+    if not helpers.get_crc16_checksum(packet[0:-2]) == packet[-2]+(packet[-1] << 8):
         return {'unused_packet': unused_packet[packet_correct_size:], 'error': 'CRC16 Not Correct'}
     # parse header
     protocol_data['seq'] = packet[3]
-    protocol_data['id'] = packet[5-skip_crc8]+(packet[6-skip_crc8]<<8)
+    protocol_data['id'] = packet[5-skip_crc8]+(packet[6-skip_crc8] << 8)
     # parse data
-    protocol_data['name'] = next(x for x,y in protocol_info_table.items() if y.get('id','')==protocol_data['id'])
-    protocol_data['data'] = packet_to_dict(protocol_data['name'], packet[7-skip_crc8:-2])
+    protocol_data['name'] = next(x for x, y in protocol_info_table.items(
+    ) if y.get('id', '') == protocol_data['id'])
+    protocol_data['data'] = packet_to_dict(
+        protocol_data['name'], packet[7-skip_crc8:-2])
     return {'unused_packet': unused_packet, 'result': protocol_data}
+
 
 def pack(name, data, seq=1, skip_crc8=False):
     protocol_info = protocol_info_table[name]
@@ -90,15 +101,17 @@ def pack(name, data, seq=1, skip_crc8=False):
         packet += bytes([helpers.get_crc8_checksum(packet.tobytes())])
     packet += bitstring.pack('<H', protocol_info['id'])
     packet += dict_to_packet(name, data)
-    packet += bitstring.pack('<H', helpers.get_crc16_checksum(packet.tobytes()))
+    packet += bitstring.pack('<H',
+                             helpers.get_crc16_checksum(packet.tobytes()))
     return packet.tobytes()
+
 
 class NodeBridgeProtocol():
     def __init__(self, source_list=None):
         self.data = {}
         self.seq = 0
         self.packet_buffer = bytes()
-    
+
     def update(self, packet, verbose=False):
         self.packet_buffer += packet
         unpack_result = unpack(self.packet_buffer)
@@ -112,7 +125,8 @@ class NodeBridgeProtocol():
             if 'error' in unpack_result:
                 print('ERR Protocol: ', unpack_result['error'])
             elif 'result' in unpack_result:
-                print('LOG Protocol: ', unpack_result['result']['name'], unpack_result['result']['data'])
+                print(
+                    'LOG Protocol: ', unpack_result['result']['name'], unpack_result['result']['data'])
         return unpack_result
 
 
@@ -124,4 +138,6 @@ if __name__ == '__main__':
     test_data['yaw_angle_diff'] = 2.22
     test_packet = pack('autoaimData', test_data)
     print('pack result: ', test_packet)
-    print('unpack result: ', unpack(bytes([1,2,3])+test_packet+bytes([4,5,6]))) # test with some useless bytes
+    # test with some useless bytes
+    print('unpack result: ', unpack(
+        bytes([1, 2, 3])+test_packet+bytes([4, 5, 6])))
